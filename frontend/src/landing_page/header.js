5775c5c6012bb516d13bc9d42276d1642ec564c8
EricVara <vara.ada91@gmail.com>
2022-04-06 01:17:19 -0700

    frontend structure update

---
+++ frontend/src/landing_page/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function branding() {
+  return (
+    <div className="brand">
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <div></div>
+    </div>
+  );
+}
