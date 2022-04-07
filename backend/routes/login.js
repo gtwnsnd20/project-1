63a0492ca1208a98ddc89feef5543361259806f0
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-04-06 22:26:13 -0500

    updated login and register on backend

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,20 +4,20 @@ const pool = require('../api/database');
 const router = express.Router();
 
 router.post('/', (req,res) => {
-  userObj = req.body;
+  let userObj = req.body;
   console.log(JSON.stringify(userObj));
-  let user_name = userObj.username;
-  let pass = userObj.password;
-  console.log(user_name)
-  pool.query('SELECT username,hashed_password FROM users WHERE username=$1',[user_name],(error,results)=>{//Query server for user
+  let username = userObj.username;
+  let password = userObj.password;
+  console.log(username);
+  pool.query('SELECT username,password FROM users WHERE username=$1',[username],(error,results)=>{//Query server for user
       if(error){//If uname not found, return error
           console.log(`Login Query error: ${error}`);
           res.status(403).json(error);
       } else {
           let data = results.rows[0];
-          console.log(data)
-          console.log("pass:" +pass + " password:" + data.hashed_password)
-          if(pass == data.hashed_password){//If password is correct
+          console.log(data);
+          console.log("password:" + password);
+          if(password == data.password){//If password is correct
               console.log("Password is correct")
               res.status(200).json();
           }   else {//Send status for password incorrect
@@ -25,8 +25,6 @@ router.post('/', (req,res) => {
               res.status(403).json();//send password incorrect
           }
       }
-
-
   })
 });
 
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -3,4 +3,29 @@ const pool = require('../api/database');
 
 const router = express.Router();
 
+router.post('/', (req,res) => {
+  let {username, password, email} = req.body;
+  console.log([username,password,email]);
+  pool.query('SELECT * FROM users WHERE username=$1', [username], (error,results) => { // Query to check for username already taken
+    if(error) {
+      console.log(`User Table Check Query error: ${error}`);
+        res.status(403).json(error);
+    }
+
+    if(results !== []){ //Throws error if username found in DB
+      res.status(409).send('Username already taken');
+    } else { // Else insert new user
+      pool.query('INSERT INTO users VALUES(DEFAULT,$1,$2,$3)', [username,password,email], (error2,results2) => { // Query to insert new user
+        if(error2){
+          console.log(`Registration Query error: ${error2}`);
+            res.status(403).json(error);
+        }
+      
+        res.status(201).send('New user added successfully');
+      });
+    }
+  });
+
+});
+
 module.exports = router;
