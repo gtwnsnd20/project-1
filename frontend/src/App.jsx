fdbd3b4c60d8fcde53111e19eade2dd372b9bef1
ysabeau <kaila.1321@gmail.com>
2022-04-07 09:34:21 -0400

    added react-bootstrap reference and basic footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
       <h1>ReactIT</h1>
+      <Footer />
     </div>
   );
 }
