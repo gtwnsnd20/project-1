09306ffad85650795c96a91df534fffb1cb9040d
hrisse <hunterrisse@gmail.com>
2022-04-07 23:48:24 -0400

    delete obsolete code

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/',(req,res)=>{
     pool.query('SELECT name FROM category',(error,results)=>{
-        if(error){//throw error if categories not found
+        if(error){
             throw error;
             res.status(403).json(error);
         } else {
