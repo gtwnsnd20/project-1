e34afbcd6ee86a69794378f5d25f671cfa795276
hrisse <hunterrisse@gmail.com>
2022-04-08 21:36:32 -0400

    change password length and role table to roles

--- SQL queries.sql
+++ SQL queries.sql
@@ -1,19 +1,27 @@
-CREATE TABLE role (role_id serial PRIMARY KEY, role_name VARCHAR(15));
-CREATE TABLE users (user_id serial PRIMARY KEY, username varchar(25),password varchar(30), email varchar(30),role_id int DEFAULT 1,register_date TIMESTAMP DEFAULT CURRENT_DATE,last_login TIMESTAMP DEFAULT CURRENT_DATE, CONSTRAINT fk_role_ID FOREIGN KEY(role_id) REFERENCES role(role_id));
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS thread CASCADE;
+DROP TABLE IF EXISTS post CASCADE;
+DROP TABLE IF EXISTS role CASCADE;
+DROP TABLE IF EXISTS category CASCADE;
+DROP TABLE IF EXISTS roles CASCADE;
+
+--Create Tables
+CREATE TABLE roles (role_id serial PRIMARY KEY, role_name VARCHAR(15));
+CREATE TABLE users (user_id serial PRIMARY KEY, username varchar(25),role_id int,password varchar(60), email varchar(30),register_date TIMESTAMP DEFAULT CURRENT_DATE,last_login TIMESTAMP DEFAULT CURRENT_DATE, CONSTRAINT fk_role_ID FOREIGN KEY(role_id) REFERENCES roles(role_id));
 CREATE TABLE category (cat_id serial PRIMARY KEY, name VARCHAR(120));
 CREATE TABLE thread (thread_id serial PRIMARY KEY, cat_id INT REFERENCES category (cat_id),subject VARCHAR(120), user_id INT,create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE SET NULL);
 CREATE TABLE post (post_id serial PRIMARY KEY, thread_id INT REFERENCES thread (thread_id) ON DELETE CASCADE, user_id INT REFERENCES users (user_id) ON DELETE SET NULL, content VARCHAR(5000) NOT NULL, post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 --add user roles
-INSERT INTO role (role_name) VALUES ('user');
-INSERT INTO role (role_name) VALUES ('admin');
+INSERT INTO roles (role_name) VALUES ('user');
+INSERT INTO roles (role_name) VALUES ('admin');
 
 --Insert Users
-INSERT INTO users VALUES (DEFAULT,'hunterrisse','SADJLFAIMSCIMASE','hunterrisse@gmail.com',2);
-INSERT INTO users VALUES (DEFAULT,'Madison','SADJLFAIMSCIMASE','M@gmail.com',2);
-INSERT INTO users VALUES (DEFAULT,'Gino','SADJLFAIMSCIMASE','Gilgamesh@gmail.com',2);
-INSERT INTO users VALUES (DEFAULT,'Eric','SADJLFAIMSCIMASE','Evara@gmail.com',2);
-INSERT INTO users VALUES (DEFAULT,'Plebian','SADJLFAIMSCIMASE','Evara@gmail.com');
+INSERT INTO users VALUES (DEFAULT,'hunterrisse',2,'SADJLFAIMSCIMASE','hunterrisse@gmail.com');
+INSERT INTO users VALUES (DEFAULT,'Madison',2,'SADJLFAIMSCIMASE','M@gmail.com');
+INSERT INTO users VALUES (DEFAULT,'Gino',2,'SADJLFAIMSCIMASE','Gilgamesh@gmail.com');
+INSERT INTO users VALUES (DEFAULT,'Eric',2,'SADJLFAIMSCIMASE','Evara@gmail.com');
+
 --insert categories
 INSERT INTO category VALUES(DEFAULT,'Anime');
 INSERT INTO category VALUES(DEFAULT,'Technology');
@@ -25,11 +33,6 @@ INSERT INTO thread VALUES (DEFAULT,1,'Best Anime',1,DEFAULT);
 INSERT INTO thread VALUES (DEFAULT,2,'React technology',4,DEFAULT);
 INSERT INTO thread VALUES (DEFAULT,3,'Playing as Japan psychology',3,DEFAULT);
 INSERT INTO thread VALUES (DEFAULT,4,'Morning art',2,DEFAULT);
-INSERT INTO thread VALUES (DEFAULT,1,'Jujutsu Kaisen Reviews',1,DEFAULT);
-INSERT INTO thread VALUES (DEFAULT,2,'Having Fun Learning JAVA!!',4,DEFAULT);
-INSERT INTO thread VALUES (DEFAULT,3,'The facts about Depression',3,DEFAULT);
-INSERT INTO thread VALUES (DEFAULT,4,'Facts about Pablo Picasso',2,DEFAULT);
-
 
 --make post
 INSERT INTO post VALUES (DEFAULT,1,1,'Anime is the pinnacle of entertaiment, The best of the best being Kanata no Astra: Lost in Space. CHANGE MY MIND',DEFAULT);
@@ -41,9 +44,3 @@ INSERT INTO post VALUES (DEFAULT,1,1,'You can say that after you see Kanata no A
 
 SELECT content,post_date,user_id FROM post WHERE thread_id = (select thread_id FROM thread WHERE subject = 'Best Anime');
 
---drop tables
-DROP TABLE users CASCADE;
-DROP TABLE thread CASCADE;
-DROP TABLE post CASCADE;
-DROP TABLE role;
-DROP TABLE category CASCADE;
