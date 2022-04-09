c849b6254a50feea5eefcdd25b39bb732bb520ec
ysabeau <kaila.1321@gmail.com>
2022-04-09 00:31:45 -0400

    demo tabbed forrum for landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ForumTabs from './components/TabForums';
 import './style.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ForumTabs />
       <Footer />
     </div>
   );
