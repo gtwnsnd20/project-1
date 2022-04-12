4664dfcf15daabf79fd3574fa6870ee5bf7bb6c4
hrisse <hunterrisse@gmail.com>
2022-04-11 23:09:49 -0400

    add category form validation

--- backend/routes/add_category.js
+++ backend/routes/add_category.js
@@ -3,16 +3,24 @@ const pool = require('../api/database');
 
 const router = express.Router();
 
+function validateCategory(catName){
+    return !!(catName)
+}
+
 router.post('/',(req,res)=>{
     let name = req.body.catName;
     console.log(req.body);
     console.log(name);
-    pool.query('INSERT INTO category VALUES (DEFAULT,$1)',[name],(error,result)=>{
+    if (validateCategory(name)){
+          pool.query('INSERT INTO category VALUES (DEFAULT,$1)',[name],(error,result)=>{
         if (error) {
             console.log(`INSERT INTO Category Query Error: ${error}`);
-          }
-      
+        }
           res.status(201).send('New category created successfully!');
     })
+    } else {
+        res.status(403).send("Improper Form")
+    }
+  
 })
 module.exports = router;
