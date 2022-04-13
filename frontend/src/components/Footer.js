58373ecf77b44f214ca8c9af317491605412c621
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-04-12 21:04:22 -0500

    testing axios on login

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="text-center text-lg-start text-white">
-      <div class="text-center p-4">
+    <footer className="text-center text-lg-start text-white">
+      <div className="text-center p-4">
         © 2022 Copyright
       </div>
     </footer>
