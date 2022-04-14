0e507b58534090282b85f43e666a40bd01ee329a
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-04-13 23:52:56 -0500

    updated Pages folder directory

--- frontend/src/components/Pages/Home.js
+++ frontend/src/components/Pages/Home.js
@@ -1,5 +1,4 @@
 import Navbar from "../components/Navbar";
-//import ForumTabsDemo from "../components/TabForums";
 import Categories from "../components/Categories"
 import Footer from "../components/Footer";
 
---
+++ frontend/src/components/Pages/LoginPage.js
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar";
+import LoginIntegration from "../LoginIntegration";
+
+function LoginPage(){
+    return (
+        <>
+        <Navbar />
+        <LoginIntegration/>
+        </>
+    )
+}
+export default LoginPage;
