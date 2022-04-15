7015b335c9dfd2ceec50757c8ac3aa67857e0eac
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-04-15 11:28:02 -0500

    initial commit

---
+++ frontend/src/components/Logout.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Logout() {
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/logout');
+    document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    navigate(-1, {replace: true});
+  }, [navigate]);
+
+  return(
+    <></>
+  );
+
+}
+
+export default Logout;
