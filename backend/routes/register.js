ef05b8969570cfdee7e66875d201998c97415bf6
hrisse <hunterrisse@gmail.com>
2022-04-15 13:31:39 -0400

    add auto login upon registering new user

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,8 +1,9 @@
 const express = require('express');
 //const bcrypt = require("bcryptjs")
 const pool = require('../api/database');
-
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const one_day = 24 * 60 * 60 * 1000;
 
 function validateRegister(username,password,email){
   return !!(username && password && email);
@@ -24,13 +25,21 @@ router.post('/', (req,res) => {
       } else { // Else insert new user
         //hashPassword = bcrypt.hashSync(password,10);
         //console.log(hashPassword);
-        pool.query('INSERT INTO users VALUES(DEFAULT,$1,$2,$3)', [username,password,email], (error2,results2) => { // Query to insert new user
+        pool.query('INSERT INTO users VALUES(DEFAULT,$1,$2,$3) RETURNING *', [username,password,email], (error2,results2) => { // Query to insert new user
           if(error2){
             console.log(`Registration Query error: ${error2}`);
               res.status(403).send("Query Error");
           }
-        
-          res.status(201).send('New user added successfully');
+          console.log(results2.rows)
+          console.log(results2.rows[0])
+          let user_id = results2.rows[0].user_id;
+          let isAdmin = results2.rows[0].admin;
+          const token = jwt.sign({user:username,userid:user_id,isadmin:isAdmin},process.env.TOKEN_SECRET,{expiresIn: '24h'});
+          res.status(200).cookie("access_token", [token,username, isAdmin],  {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: one_day
+        }).json([token,username,user_id,isAdmin]);
         });
       }
   });
