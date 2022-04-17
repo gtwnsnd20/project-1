ddf384846ae36d2899f14ea57478729042884678
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-04-17 00:55:01 -0500

    made ThreadForm modal

--- frontend/src/components/ThreadForm.js
+++ frontend/src/components/ThreadForm.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
-import { Windows } from "react-bootstrap-icons";
-import { Link } from "react-router-dom";
+import { Modal, Form, Button } from "react-bootstrap";
 import getCookie from "./Utils/getCookie";
 
 //need to connect to database to get thread ids and add new threads to db
@@ -10,105 +8,120 @@ import getCookie from "./Utils/getCookie";
 
 
 function ThreadForm(props) {
-  const [thread_description, setThread_description] = useState();
-  const [cat_id, setCat_id] = useState();
-  const [errorMessage, setErrorMessage] = useState(false);
-  const [titleError, setTitleError] = useState(false);
-  const [catError, setCatError] = useState (false);
-  const [descError,setDescError] = useState (false);
-  const categories = props.categories
-  
+  const categories = props.categories;
 
-  //function to handle submit event --incomplete for now
+  // state + functions for showing/hiding form
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+  const [catId, setCatId] = useState();
+
+  const [catError, setCatError] = useState({});
+  const [titleError, setTitleError] = useState({});
+  const [descError, setDescError] = useState({});
+
+  
+  const errors = {
+    category: 'Must select category',
+    title: 'Title must be at least 3 characters',
+    description: 'Description must be at least 3 characters'
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    setCatError({});
+    setTitleError({});
+    setDescError({});
+
     const authCookie = getCookie();
-    setErrorMessage(false);//Reset rrorMessages
-    console.log(errorMessage)
-    const subject = document.forms[0].subject.value;//Because input is a controlled component, we must get the value from forms
-
+    let user_id = authCookie.userid;
     
+    let {subject, description} = document.forms[0];
 
-      let username= authCookie.username;//Store user information from cookie
-      let user_id = authCookie.userid;
-    const params = {cat_id,subject,thread_description,user_id};
-    setTitleError(false);
-    setCatError(false);
-    setDescError(false);
-    setErrorMessage(false);
+    const params = {
+      cat_id: catId,
+      subject: subject.value,
+      thread_description: description.value,
+      user_id: user_id
+    }
 
+    if(isNaN(catId)){//Check if category selected
+      setCatError({name: 'category', message: errors.category});
+    }
+    if(params.subject === undefined || params.subject.length < 3){//Check if title is long enough
+      setTitleError({name: 'title', message: errors.title});
+    }
+    if(params.thread_description === undefined || params.thread_description.length < 3){//Check if thread is long enough
+      setDescError({name: 'description', message: errors.description});
+    }
 
-    if(subject.length < 3){//Check if title is long enough
-      setErrorMessage(true);
-      setTitleError(true);
-    }
-    if(isNaN(cat_id)){//Check if category selected
-      setErrorMessage(true);
-      setCatError(true);
-    }
-    if(thread_description == ''|| thread_description == undefined){//Check if thread is long enough
-      setErrorMessage(true);
-      setDescError(true);
-    } else if(thread_description.length < 3){
-      setErrorMessage(true);
-      setDescError(true);
-    }
-    console.log(errorMessage)
-    if(!errorMessage){
-      axios.post('http://localhost:3001/add-thread',params).then((res)=>{
-        console.log(res.data)
-        window.location.reload(false);//Reload page once thread is added.
-        }).catch((error)=>{
+    if( catError === {} && titleError === {} && descError === {} ){
+      axios.post('http://localhost:3001/add-thread',params).then((_res)=>{
+        window.location.reload(false); // Reload page once thread is added.
+      }).catch((error)=>{
         if(error.response.status === 400){
-          console.log("improper Form was found")
+          console.log("Improper Format");
         }
-        console.log(error)
-    })
+        console.log(error);
+      })
     }
-
-   
-
 
   };
+
+  const renderErrorMessage = (name,error_msg) => error_msg !== {} && name === error_msg.name && (
+    <div className='error'>{error_msg.message}</div>
+  );
+
   //code for new thread form with inputs for the title and description
   const renderThreadForm = (
-
-    <div className="threadForm">
-      <Form onSubmit={handleSubmit} id="newThread" className="form-newthread text-center">
-        <Form.Group className="mb-3" controlId="formTitle">
-          <Form.Label>Thread Title:</Form.Label>
-          <Form.Control type="text" placeholder="Enter title" name="subject"/>
-        </Form.Group>
-        {titleError && <p>Title must be at least 3 characters</p>}
-        <Form.Group className="mb-3" controlId="cat_id">
-          <Form.Label>Category</Form.Label>
-          <Form.Select value={cat_id} onChange={(e)=> setCat_id(e.target.value)}>
-          <option>Select </option>
-          {categories.map((cat_id)=>(<option key={cat_id.cat_id} value={cat_id.cat_id}>{cat_id.name}</option>))}
-          </Form.Select>
-          
-        </Form.Group>
-        {catError && <p>Must Select Category</p>}
-        <br></br>
-        <Form.Group className="mb-3" controlId="threadDescriptoin">
-          <Form.Label>Description:</Form.Label>
-          <Form.Control as="textarea" style={{height:'100px'}} placeholder="Enter brief description" name='thread_description' value={thread_description} onChange={(e)=>setThread_description(e.target.value)}/>
-        </Form.Group>
-        {descError && <p>Description must be at least 3 characters</p>}
-        <Button variant="info" type="submit" className="justify-content-center">Submit</Button>
-      </Form>
-    </div>
+    <Modal className="threadForm" show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Create New Thread</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form id="newThread" className="form-newthread text-center">
+          <Form.Group className="mb-3" controlId="catId">
+            <Form.Label>Category</Form.Label>
+            <Form.Select value={catId} onChange={(e)=> setCatId(e.target.value)}>
+            <option>Select </option>
+            {categories.map((cat_id)=>(<option key={cat_id.cat_id} value={cat_id.cat_id}>{cat_id.name}</option>))}
+            </Form.Select>
+          </Form.Group>
+          {renderErrorMessage('category',catError)}
+          <br/>
+          <Form.Group className="mb-3" controlId="formTitle">
+            <Form.Label>Thread Title:</Form.Label>
+            <Form.Control type="text" placeholder="Enter title" name="subject"/>
+          </Form.Group>
+          {renderErrorMessage('title',titleError)}
+          <br/>
+          <Form.Group className="mb-3" controlId="threadDescription">
+            <Form.Label>Description:</Form.Label>
+            <Form.Control as="textarea" style={{height:'100px'}} placeholder="Enter a brief description" name='description'/>
+          </Form.Group>
+          {renderErrorMessage('description',descError)}
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit Thread
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 
   return (
-    <>
-      <div className="createthread">
-        <div className="newformtitle">Create New Thread</div>
-        <div>
-          {renderThreadForm}
-        </div>
-      </div>
-    </>
+    <div>
+      <Button variant='primary' onClick={handleShow}>
+        Create New Thread
+      </Button>
+
+      {renderThreadForm}
+    </div>
   );
 };
 
