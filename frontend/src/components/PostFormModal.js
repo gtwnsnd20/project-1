b83b0a42313ced7c7342fd542a32244855a3a7c0
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-04-16 22:54:29 -0500

    fixed closing form, cleaned codebase

--- frontend/src/components/PostFormModal.js
+++ frontend/src/components/PostFormModal.js
@@ -4,64 +4,60 @@ import axios from 'axios';
 import getCookie from "./Utils/getCookie";
 axios.defaults.withCredentials = true;
 
-//Store username and userId from cookie
-
-
-//Actual Component
 function PostFormModal(props) {
 
-  
-
-  console.log(props.thread_id)
+  // state + functions for showing/hiding form
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const [contentError, setContentError] = useState(false);
+
+  // state for rendering error message
+  const [errorMessage,setErrorMessage] = useState({});
+
+  const errors = {
+    length: "Text must be at least 3 characters long"
+  }
 
   const handleSubmit = (event) => {
+    event.preventDefault();
+    
     //Get cookie from Document
     const authCookie = getCookie();
-    let username = authCookie.username;
-    let user_id = authCookie.userId;
-    setContentError(false);
-
     
-    console.log(`User_ID=${user_id}`)
-    event.preventDefault();
+    let user_id = authCookie.userid;
     var { content } = document.forms[0];
-    console.log(`The variable content=${content.value}`)
-    const params = {thread_id:props.thread_id,user_id:user_id,content:content.value}//Parameters for axios call
-    
-    if(params.content === undefined || params.content === ''){
-      setContentError(true);
-    } else if(params.content.length < 3){
-      setContentError(true);
+    const params = {
+      thread_id:props.thread_id,
+      user_id:user_id,
+      content:content.value
     }
     
-    if(!contentError){//Send if length of content was good.
+    if(params.content === undefined || params.content.length < 3){ // renders content length error on submit if too small
+      setErrorMessage({name: 'length', message: errors.length});
+    } else {
       axios.post(`http://localhost:3001/add-post`,params,{headers:{ 
-
         "Access-Control-Allow-Origin" : "http://localhost:3001",
         "Access-Control-Allow-Methods":"POST,OPTIONS,GET",
         'Access-Control-Allow-Headers': 'text/plain'
-      }} ).then((res)=>{
-      window.location.reload(false);//Reload page once thread is added.
+      }}).then((_res)=>{
+        window.location.reload(false); // Reload page once thread is added.
       }).catch((error)=>{
         console.log(error);
       })
     }
-    
-
   }
 
+  const renderErrorMessage = (name) => name === errorMessage.name && (
+    <div className='error'>{errorMessage.message}</div>
+  );
 
   return (
     <div>
       <Button variant='primary' onClick={handleShow}>
-        Create Post
+        Create New Post
       </Button>
 
-      <Modal show={show} onHide={handleShow}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Create New Post</Modal.Title>
         </Modal.Header>
@@ -72,10 +68,9 @@ function PostFormModal(props) {
               <Form.Control as="textarea" rows={3} name="content" />
             </Form.Group>
           </Form>
-          {contentError && <p>Post must be at least 3 Characters Long</p>}
+          {renderErrorMessage('length')}
         </Modal.Body>
         <Modal.Footer>
-          
           <Button variant='secondary' onClick={handleClose}>
             Close
           </Button>
