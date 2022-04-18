2dd44de36fcc35ac45d2aba8df003032ba2d9a2b
EricVara <vara.ada91@gmail.com>
2022-04-18 06:10:59 -0700

    added margin between navbar and user login in api, added style to button

--- frontend/src/components/register.jsx
+++ frontend/src/components/register.jsx
@@ -1,54 +1,63 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import loginImg from './Assets/Images/chicken.png';
 
 function Register(props) {
-  
-  const [isRegistered,setIsRegistered] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false);
 
   let navigate = useNavigate();
 
   useEffect(() => {
-    if(isRegistered){
-      navigate('../', {replace: true});
+    if (isRegistered) {
+      navigate('../', { replace: true });
     }
-  }, [isRegistered,navigate]);
+  }, [isRegistered, navigate]);
 
   // tracks state of error message
-  const [errorMessage,setErrorMessage] = useState({});
+  const [errorMessage, setErrorMessage] = useState({});
 
   // set of potential error messages
   const errors = {
-    name_taken: "Username already taken",
-    server: "Server error"
-  }
+    name_taken: 'Username already taken',
+    server: 'Server error',
+  };
 
   // takes form input and sends to backend through an axios call
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    let {username,email,password} = document.forms[0];
-    
-    axios.post('http://localhost:3001/register',
-      {username:username.value, password:password.value, email:email.value}
-    ).then((response) => { // logs in user, saves cookie(to be finished)
+    let { username, email, password } = document.forms[0];
+
+    axios
+      .post('http://localhost:3001/register', {
+        username: username.value,
+        password: password.value,
+        email: email.value,
+      })
+      .then((response) => {
+        // logs in user, saves cookie(to be finished)
         console.log("You've made a new account!");
-        document.cookie = `user_token=${response.data}`
+        document.cookie = `user_token=${response.data}`;
         setIsRegistered(true);
-    }).catch((error) => { // catches and sets error message from call
-      if(error.response.status === 409) {
-        console.log(error.response.status);
-        setErrorMessage({ name: 'name_taken', message: errors.name_taken });
-      } else {
-        console.log(error.response.status);
-        setErrorMessage({ name: 'server', message: errors.server });
-      }
-    });
-  }
+      })
+      .catch((error) => {
+        // catches and sets error message from call
+        if (error.response.status === 409) {
+          console.log(error.response.status);
+          setErrorMessage({ name: 'name_taken', message: errors.name_taken });
+        } else {
+          console.log(error.response.status);
+          setErrorMessage({ name: 'server', message: errors.server });
+        }
+      });
+  };
 
   // renders error message if axios call returns error
-  const renderErrorMessage = (name) => name === errorMessage.name && (<div className='error'>{errorMessage.message}</div>);
+  const renderErrorMessage = (name) =>
+    name === errorMessage.name && (
+      <div className="error">{errorMessage.message}</div>
+    );
 
   // 1. created card register header
   // 2. created content container and declared image, form and footer classes
@@ -60,23 +69,33 @@ function Register(props) {
       <div className="header">Register</div>
       <div className="content">
         <div className="image">
-          <img src={loginImg} alt='loginImg'/>
+          <img src={loginImg} alt="loginImg" />
         </div>
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-group">
             <label className="username">Username</label>
-            <input type="text" name="username" placeholder="Username" required/>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              required
+            />
             {renderErrorMessage('name_taken')}
           </div>
           <div className="form-group">
             <label className="email">Email</label>
-            <input type="text" name="email" placeholder="E-mail" required/>
+            <input type="text" name="email" placeholder="E-mail" required />
           </div>
           <div className="form-group">
             <label className="password">Password</label>
-            <input type="text" name="password" placeholder="Password" required/>
+            <input
+              type="text"
+              name="password"
+              placeholder="Password"
+              required
+            />
           </div>
-          <button type="submit" className="btn">
+          <button type="submit" className="btnn">
             Register
           </button>
         </form>
