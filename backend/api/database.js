0fc038a49d6535753db0bb177ccd295ed4035ef1
hrisse <hunterrisse@gmail.com>
2022-04-18 11:06:32 -0400

    switch to rds

--- backend/api/database.js
+++ backend/api/database.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'project1',
-    host: 'localhost',
+    host: 'database-1.cftm6s4rkqne.us-east-1.rds.amazonaws.com',
     database: 'reactit_db',
     password: 'project1',
     port:5432
